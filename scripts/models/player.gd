bef4c376752180eecbff12a2d0a7156c07679ef0
itsAlemao <alessioginelli1@gmail.com>
2026-01-18 19:27:29 +0100

    used animation tree for attack animations

--- scripts/models/player.gd
+++ scripts/models/player.gd
@@ -1,18 +1,22 @@
 extends CharacterNode
 class_name PlayerNode
 
-@onready var _animation_player: AnimationPlayer = $AnimationPlayer
-@onready var _animation_tree: CharacterAnimationTree = $AnimationTree
+@onready var _animation_player: AnimationPlayer = $PlayerAnim
+@onready var _animation_tree: CharacterAnimationTree = $PlayerAnimTree
+@onready var _projectile: AnimatedSprite2D = $Projectile
 
 ## The player resource
 @export var _player: Player
-
-var attacking: bool = true
 
 func characterResource() -> Character:
 	return _player
 	
 func _ready() -> void:
+	_projectile.visible = true
+	_animation_tree.animation_finished.connect(func(anim_name):
+		if anim_name == "Attack - ToIdle":
+			_on_toIdle()	
+	)
 
 	var global_library = _animation_player.get_animation_library("")
 	
@@ -26,6 +30,12 @@ func _ready() -> void:
 			global_library.add_animation(a.attack.proj_anim_name(), proj_anim)
 
 	get_tree().create_timer(1.5).timeout.connect(func(): 
+		_projectile.visible = true
 		_animation_tree._travel_to_attack()
+
 		print("firing...")
 	)
+
+
+func _on_toIdle():
+	_projectile.visible = false
